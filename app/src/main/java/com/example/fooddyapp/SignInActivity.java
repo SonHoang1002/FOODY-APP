e50ec2e2355306eef549c1ea88a74c039aa6cc6a
Admin <hoangtrungson07012001@gmail.com>
2022-03-30 05:38:39 +0700

    tích hợp captcha và ẩn hiện nút sign in

--- app/src/main/java/com/example/fooddyapp/SignInActivity.java
+++ app/src/main/java/com/example/fooddyapp/SignInActivity.java
@@ -1,23 +1,37 @@
 package com.example.fooddyapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks {
     Button btnSignIn,btnSignUp;
     TextView eVEmailSI, eVPassSI;
+    CheckBox cbRobot;
+    GoogleApiClient googleApiClient;
+    String SiteKey = "6LdL9ygfAAAAAGUaQWt_OJjcAx_LDW5C4cPjIkfB";// cần đăng kí tên pj tại đường link sau
+    // https://www.google.com/recaptcha/admin/create
     private FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
@@ -29,12 +43,45 @@ public class SignInActivity extends AppCompatActivity {
         eVEmailSI = findViewById(R.id.eVEmailSI);
         eVPassSI = findViewById(R.id.eVPassSI);
         btnSignUp = findViewById(R.id.btnSignUp);
-        btnSignIn.setOnClickListener(view -> {
-            SignIn();
-        });
+
+        btnSignUp.setVisibility(View.INVISIBLE);
+
         btnSignUp.setOnClickListener(view->{
             startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
         });
+
+        cbRobot = findViewById(R.id.cbRobot);
+
+        Log.d("abc","cbRobot = findViewById(R.id.cbRobot)");
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(SafetyNet.API)
+                .addConnectionCallbacks(SignInActivity.this)
+                .build();
+
+        googleApiClient.connect();
+        cbRobot.setOnClickListener(view -> {
+            if(cbRobot.isChecked()){
+                SafetyNet.SafetyNetApi.verifyWithRecaptcha(googleApiClient,SiteKey)
+                        .setResultCallback(new ResultCallback<SafetyNetApi.RecaptchaTokenResult>() {
+                            @Override
+                            public void onResult(@NonNull SafetyNetApi.RecaptchaTokenResult recaptchaTokenResult) {
+                                Status status = recaptchaTokenResult.getStatus();
+                                if(status !=null || status.isSuccess()){
+                                    Toast.makeText(getApplicationContext(),"Successfully Verified..",Toast.LENGTH_SHORT).show();
+                                    cbRobot.setTextColor(Color.GREEN);
+                                    btnSignUp.setVisibility(View.VISIBLE);
+                                    btnSignIn.setOnClickListener(view -> {
+                                        SignIn();
+                                    });
+                                }
+                            }
+                        });
+            }else{
+
+                cbRobot.setTextColor(Color.RED);
+            }
+        });
+
     }
     private void SignIn() {
         String email = eVEmailSI.getText().toString();
@@ -59,6 +106,16 @@ public class SignInActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 }
